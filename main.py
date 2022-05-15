d607fa60e7def48b147616b0378442d93bbdd6e4
Anton Ilic <93786227+SchoIar@users.noreply.github.com>
2022-05-14 21:08:33 -0700

    added music cog, improved upon main cog

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import discord
 import asyncio
 import requests
 import random
+from discord.ext import tasks
 
 #counter = 1
 #meow
 intents = discord.Intents.all() #MUST ALSO ADD iNTENTS IN DISCORD DEV PORTAL
-bot = commands.Bot(command_prefix='.', help_command=None, intents = intents)
+bot = commands.Bot(command_prefix='.', help_command=None, intents = intents, case_insensitive=True)
 bot.remove_command("help")
+
+initial_extensions = ['cogs.music']
+for extension in initial_extensions:
+    bot.load_extension(extension)
 
 @bot.event
 async def on_ready():
@@ -42,14 +47,25 @@ async def on_message(message):
 
 @bot.command()
 async def help(ctx):
-        em = discord.Embed(title = "パグ Help", description ="", color = 0x00a8ff)
-        em.add_field(name="`$clear`", value="Clears channel message history")
-        em.add_field(name="`$rpt`", value="Make the bot echo you ")
+        em = discord.Embed(title = "M3's Help", description ="", color = 0x00a8ff)
+        em.add_field(name="`$clear`", value="Clears channel message history (admin only)")
+        em.add_field(name="`$rpt`", value="Make the bot echo you (admin only) ")
         em.add_field(name="`$dice`", value="Rolls a dice (6 faces)")
         em.add_field(name="`$add`", value="adds two numbers together!")
         em.add_field(name="`$choose`", value="chooses between multiple choices!")
+        em.add_field(name="`$helpmusic`", value="help commands for M31ON's music commands")
         em.set_footer(text="M31ON")
         await ctx.send(embed=em) 
+
+@bot.command()
+async def helpmusic(ctx):
+        em = discord.Embed(title = "Music Help", description ="", color = 0x00a8ff)
+        em.add_field(name="`$p`", value="play's a song from youtube! (uses YoutubeDl")
+        em.add_field(name="`$summon", value="Make the bot join the call ")
+        em.add_field(name="`$q`", value="Shows the queue")
+        em.add_field(name="`$skip`", value="Skips the current song")
+        em.add_field(name="`$pause/$resume`", value="Pauses/resumes the current song")
+#theres more but i cant be bothered to list them out, just check the cog LOL
 
 @bot.command(name='dice', aliases=['roll','rolladice'])
 async def dice(ctx):
@@ -86,6 +102,26 @@ async def rpt(ctx,*,message="Error: No/Invalid message entered"):
         await ctx.send(embed=embed)
     else:
         await ctx.send("insufficiant perms")
+
+## LOOP BTC amt. 
+@bot.command()
+async def startBTC(ctx): #Note: Zero is used to represent false, and One is used to represent true.
+    myLoop.start()
+    await ctx.channel.purge(limit=1)
+    await ctx.send('starting...')
+    
+@bot.command()
+async def stopBTC(ctx): #Note: Zero is used to represent false, and One is used to represent true.
+    myLoop.stop()
+    await ctx.channel.purge(limit=1)
+    await ctx.send('stopping...')
+
+@tasks.loop(seconds = 6) # repeat after every 10 seconds
+async def myLoop():
+    channel = bot.get_channel(974969930774626304) #could also make it based on ctx
+    #obv replace above w ur channel's ID
+    await channel.send('BTCprices are BLAH') 
+#Bitcoin prices JUST an example. Can use for anything. 
 
 load_dotenv()
 discordtoken= os.getenv('token')
